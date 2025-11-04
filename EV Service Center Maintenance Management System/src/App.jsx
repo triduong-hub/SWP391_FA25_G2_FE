54af50332c31aa0d0e2c18f233eb0e1f7f3f935b
triduong-hub <dinhtri11012005@gmail.com>
2025-11-04 21:43:14 +0700

    google

--- EV Service Center Maintenance Management System/src/App.jsx
+++ EV Service Center Maintenance Management System/src/App.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google"; // ✅ Thêm dòng này
 import AppRoute from "./pages/routers/AppRoute.jsx";
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen">
-        <AppRoute />
-      </div>
-    </Router>
+    // ✅ Bọc toàn bộ ứng dụng bằng GoogleOAuthProvider
+     <GoogleOAuthProvider clientId="361505380119-qeakq8e54al6hn7os2plfm8uo608fdtd.apps.googleusercontent.com">
+      <Router>
+        <div className="min-h-screen">
+          <AppRoute />
+        </div>
+      </Router>
+    </GoogleOAuthProvider>
   );
 }
 
 export default App;
-
